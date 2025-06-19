174707cef564d54ceb988cc58119a71c9e35b10b
Palak Mac <apple@Palak-Mac.local>
2025-06-19 19:54:43 +0530

    addinterview

---
+++ utils/schema.js
@@ -0,0 +1,12 @@
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+
+export const MockInterview = pgTable("mock_interview", {
+  id: serial("id").primaryKey(),
+  mockId: varchar("mock_id", { length: 36 }).notNull(), // UUIDs are 36 chars
+  jsonMockResp: text("json_mock_resp").notNull(),
+  jobPosition: varchar("job_position", { length: 255 }).notNull(),
+  jobDesc: varchar("job_desc", { length: 255 }).notNull(),
+  jobExperience: varchar("job_experience", { length: 10 }).notNull(),
+  createdBy: varchar("created_by", { length: 255 }).notNull(),
+  createdAt: varchar("created_at", { length: 20 }), // optional format YYYY-MM-DD
+});
